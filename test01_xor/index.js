e7ed1dbbbaffd2d7bed5a4f9de8c5b143f393771
Latheesan Kanesamoorthy <latheesan.kanesamoorthy@mvfglobal.com>
2019-10-15 16:04:44 +0100

    Updated test01

--- test01_xor/index.js
+++ test01_xor/index.js
@@ -1,5 +1,10 @@
 const brain = require('brain.js');
-const neuralNetwork = new brain.NeuralNetwork();
+const neuralNetwork = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+/**
+ * When the input differs, the output is 1
+ * When the input is same, the output is 0
+ */
 
 const trainingData = [
     { input: [0, 0], output: [0] },
@@ -8,6 +13,11 @@ const trainingData = [
     { input: [1, 1], output: [0] }
 ];
 
-neuralNetwork.train(trainingData);
+neuralNetwork.train(trainingData, {
+    log: error => console.log(error),
+    logPeriod: 100
+});
 
 console.log(neuralNetwork.run([0, 0]));
+console.log(neuralNetwork.run([1, 0]));
+console.log(neuralNetwork.run([1, 1]));
